f90f916857cf093438a02a9fe83ba846050d5358
LambdaXymox <lambda.xymox@gmail.com>
2020-04-26 15:07:22 -0500

    Fix an unused rsult warning in the to_writer function.

--- src/lib.rs
+++ src/lib.rs
@@ -553,7 +553,7 @@ pub fn to_writer<W: io::Write + io::Seek>(writer: W, atlas: &TextureAtlas2D) -> 
 
     // Write out the coordinate charts.
     zip_file.start_file("coordinate_charts.json", options)?;
-    serde_json::to_writer_pretty(&mut zip_file, &atlas.origin);
+    serde_json::to_writer_pretty(&mut zip_file, &atlas.origin)?;
     serde_json::to_writer_pretty(&mut zip_file, &atlas.coordinate_charts())?;
 
     // if the origin is the bottom left of the image, we need to flip the image back over
